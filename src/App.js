436adb59b4f943a7d3b3e04a0d00eeb299298c70
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2021-03-06 15:20:39 -0600

    avanceÂ

--- src/App.js
+++ src/App.js
@@ -1,58 +1,19 @@
+import React from 'react';
+
 import "./scss/App.scss";
-import "aframe";
-import { Entity, Scene } from "aframe-react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Hueso from "../src/views/Hueso"
 
 function App() {
   return (
     <div className="App">
-      <h1>XYZ</h1>
-      <Scene className="scene">
-        <a-assets>
-          <img
-            id="groundTexture"
-            src="https://cdn.aframe.io/a-painter/images/floor.jpg"
-          />
-          <img
-            id="skyTexture"
-            src="https://cdn.aframe.io/a-painter/images/sky.jpg"
-          />
-        </a-assets>
-        <Entity primitive="a-light" type="ambient" color="#445451" />
-        <Entity
-          primitive="a-light"
-          type="point"
-          intensity="2"
-          position="2 8 4"
-        />
-        <Entity
-          primitive="a-sky"
-          height="2048"
-          radius="30"
-          src="#skyTexture"
-          theta-length="90"
-          width="2048"
-        />
-        <Entity
-          primitive="a-plane"
-          src="#groundTexture"
-          rotation="-90 0 0"
-          height="100"
-          width="100"
-        />
-        <Entity
-          geometry={{ primitive: "box" }}
-          material={{ color: "red" }}
-          position={{ x: 0, y: 0, z: -5 }}
-        />
-        <Entity particle-system={{ preset: "snow" }} />
-        <Entity light={{ type: "point" }} />
-        <Entity gltf-model={{ src: "virtualcity.gltf" }} />
-        <Entity text={{ value: "Hello, WebVR!" }} />
-        <Entity primitive="a-box" color="red" position="0 0 -5" />
-        <Entity primitive="a-sphere" color="green" position="-2 0 -3" />
-        <Entity primitive="a-cylinder" color="blue" position="2 0 -3" />
-        <Entity primitive="a-sky" src="sechelt.jpg" />v
-      </Scene>
+      <Router>
+      <Switch>
+      <Route path="/" exact>
+              <Hueso/>
+            </Route>
+      </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/views/Hueso.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import ShitHappens from "../models/scene.gltf";
+
+// import campo from "../img/equ.jpg";
+
+const Hueso = () => {
+  console.log("hola");
+  return (
+    <section className="Hueso">
+      <div className="scene">
+        <>
+          <a-scene >
+            <a-camera position="2 6 2"></a-camera>
+            <a-assets>
+              {/* <img id="campo" src={campo} alt="" /> */}
+
+              <a-asset-item id="shit" src={ShitHappens}></a-asset-item>
+            </a-assets>
+
+            <a-entity
+              environment="active: true; skyType: gradient; skyColor: #89c2ff; horizonColor: #9ec3ea; lightPosition: 1 4 1s; fog: 0.7; flatShading: true; ground: canyon; groundYScale: 3.2699999999999994; groundTexture: checkerboard; groundColor: #fcfcfc; groundColor2: #afa6bb; dressing: stones; dressingAmount: 100; dressingColor: #444343; dressingScale: 5.2; dressingVariance: [object Object];  dressingOnPlayArea: 0.06; shadowSize: 10.52; preset: starry"
+              animation__cielo="property:environment.skyColor; to: #0562c5; dur: 8000; easing: linear; loop: true; dir: alternate"
+              
+            />
+            
+
+            <a-entity
+              gltf-model={ShitHappens}
+              scale="3 3 3"
+              position="0 0 -23.8"
+            
+            />
+          </a-scene>
+        </>
+      </div>
+    </section>
+  );
+};
+export default Hueso;
