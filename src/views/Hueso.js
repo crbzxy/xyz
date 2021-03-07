d2d49469f99562ee5d9e198b472925d980e2c47d
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2021-03-06 18:43:46 -0600

    reset

--- src/views/Hueso.js
+++ src/views/Hueso.js
@@ -6,7 +6,6 @@ import ShitHappens from "../models/scene.gltf";
 
 const Hueso = () => {
   console.log("hola");
-
   return (
     <section className="Hueso">
       <h1>xyz</h1>
@@ -16,24 +15,16 @@ const Hueso = () => {
             <a-camera position="2 6 2"></a-camera>
             <a-assets>
               {/* <img id="campo" src={campo} alt="" /> */}
-              <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg"/>
+
               <a-asset-item id="shit" src={ShitHappens}></a-asset-item>
             </a-assets>
-            <a-sky id="bg"  opacity="0.6" radius="325" src="#skyTexture" theta-length="90"></a-sky>
+
             <a-entity
-              environment="active:  true;  skyType:  gradient;  skyColor:  #89c2ff;  horizonColor:  #9ec3ea;   fog:  0.3;  flatShading:  true;  ground:  canyon;  groundYScale:  3.27;  groundTexture:  squares;  groundColor:  #f1f1f1;  groundColor2:  #d1d1d1;  dressing:  stones;  dressingAmount:  100;  dressingColor:  #5a5a5a;  dressingScale:  2.2;  dressingVariance:  1 1 1;  dressingOnPlayArea:  0.07;  shadowSize:  10.52;  preset:  starry; grid:none;
-              "
-              scale="2.9 2.2 2.7"
-              
+              environment="active: true; skyType: gradient; skyColor: #89c2ff; horizonColor: #9ec3ea; lightPosition: 1 4 1s; fog: 0.7; flatShading: true; ground: canyon; groundYScale: 3.2699999999999994; groundTexture: checkerboard; groundColor: #fcfcfc; groundColor2: #adadad; dressing: stones; dressingAmount: 100; dressingColor: #5a5a5a; dressingScale: 3.2; dressingVariance: [object Object];  dressingOnPlayArea: 0.07; shadowSize: 10.52; preset: starry"
+            
             />
             
-            <a-entity
-            light="type:ambient;  position:0 144 0; intensity:0.4;"
-            >
-
-            </a-entity>
-            
-            
+            <a-entity light=""></a-entity>
             <a-entity
               gltf-model={ShitHappens}
               scale="3 3 3"
